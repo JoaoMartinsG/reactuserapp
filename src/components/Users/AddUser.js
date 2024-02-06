7db055b7f97cc369d41a69afb4e8d2a7ebb2864d
Joao Martins <JoaoMGWork@hotmail.com>
2024-02-06 12:05:39 +0000

    Switched from useState and updating every keystroke to useRef and get value on submit only

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 
 import styles from './AddUser.module.css'
 import Card from '../UI/Card'
@@ -7,36 +7,33 @@ import ErrorModal from '../UI/ErrorModal'
 import Wrapper from '../Helpers/Wrapper'
 
 const AddUser = (props) => {
-  const [enteredUsername, setEnteredUsername] = useState('')
-  const [enteredAge, setEnteredAge] = useState('')
+  const nameInputRef = useRef()
+  const ageInputRef = useRef()
+
   const [error, setError] = useState()
 
   const addUserHandler = (event) => {
     event.preventDefault()
-    if (enteredUsername.trim().length === 0 || enteredAge.trim().lenght === 0) {
+    const enteredName = nameInputRef.current.value
+    const enteredUserAge = ageInputRef.current.value
+    if (enteredName.trim().length === 0 || enteredUserAge.trim().lenght === 0) {
       setError({
         title: 'Invalid Input',
         message: 'Please enter a valid name and age (no empty values).',
       })
       return
     }
-    if (enteredAge < 1) {
+    if (enteredUserAge < 1) {
       setError({
         title: 'Invalid age',
         message: 'Please enter a valid age (> 0).',
       })
       return
     }
-    props.onAddUser(enteredUsername, enteredAge)
-    setEnteredAge('')
-    setEnteredUsername('')
-  }
-
-  const usernameChangeHandler = (event) => {
-    setEnteredUsername(event.target.value)
-  }
-  const ageChangeHandler = (event) => {
-    setEnteredAge(event.target.value)
+    props.onAddUser(enteredName, enteredUserAge)
+    // avoid this , manipulating DOM without React, ok in this ccase since its user input field
+    nameInputRef.current.value = ''
+    ageInputRef.current.value = ''
   }
 
   const errorHandler = () => {
@@ -55,19 +52,9 @@ const AddUser = (props) => {
       <Card className={styles.input}>
         <form className={styles.form} onSubmit={addUserHandler}>
           <label htmlFor="username">Username</label>
-          <input
-            id="username"
-            type="text"
-            value={enteredUsername}
-            onChange={usernameChangeHandler}
-          />
+          <input id="username" type="text" ref={nameInputRef} />
           <label htmlFor="age">Age</label>
-          <input
-            id="age"
-            type="number"
-            value={enteredAge}
-            onChange={ageChangeHandler}
-          />
+          <input id="age" type="number" ref={ageInputRef} />
           <Button type="submit">Add User</Button>
         </form>
       </Card>
